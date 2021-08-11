e1c35e8634ae52fbbdb68abcf0ceed3854be9e5f
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-08-10 17:49:53 -0700

    commit tree fc3c95b1518e5862ab566330c16115dffe812da3

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,28 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({ bio, links }) {
+  console.log(bio, links);
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio ? <p>{bio}</p> : ""}
+      {/* <p>{props.bio}</p> */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links linkedin={links.linkedin} github={links.github} />
       {/* add your <Links /> component here */}
     </div>
   );
 }
 
 export default About;
+
+//tenary
+// if (theThinf) {
+//   "dothis()";
+
+// }
+// else {
+//   "dothat ()";
+// }
+// const result = thething ? "dothis" : "dothat"
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import data from "../data/user";
 
 function Home(props) {
   return (
